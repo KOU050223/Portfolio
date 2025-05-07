6a17d67248f53417b58efaf6c7af8f01e513ee19
KOU050223 <kouhei20050223@icloud.com>
2025-05-07 21:31:08 +0900

    add ページ追加

---
+++ src/pages/Skill.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { 
+  Box, 
+  Flex, 
+  Heading, 
+  Spacer, 
+  Button, 
+  HStack, 
+  VStack,
+  Circle,
+  Text
+} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import { useColorModeValue } from "../components/ui/color-mode";
+
+const Skill = () => {
+  const bgColor = useColorModeValue('white', 'gray.800')
+  const borderColor = useColorModeValue('gray.200', 'gray.600')
+  const lineColor = useColorModeValue('gray.300', 'gray.500')
+
+  // if (isLoading) {
+  //   return (
+  //     <Box p={8}>
+  //       <Heading size="lg" mb={8}>キャリア</Heading>
+  //       <Text>読み込み中...</Text>
+  //     </Box>
+  //   )
+  // }
+  
+  // if (error) {
+  //   return (
+  //     <Box p={8}>
+  //       <Heading size="lg" mb={8}>キャリア</Heading>
+  //       <Text color="red.500">エラーが発生しました</Text>
+  //     </Box>
+  //   )
+  // }
+
+  return (
+    <Box maxW="1200px" mx="auto" p={8}>
+      <Heading size="lg" mb={8}>技術スタック</Heading>
+      
+      <Box position="relative">
+        {/* タイムライン垂直ライン */}
+        <Box
+          position="absolute"
+          left="20px"
+          top="0"
+          bottom="0"
+          width="2px"
+          bg={lineColor}
+        />
+        
+      </Box>
+    </Box>
+  )
+}
+
+export default Skill
