22beeeb1ee9b9accb35e92fcf5fc5f2351e8f67d
KOU050223 <kouhei20050223@icloud.com>
2025-05-13 21:15:09 +0900

    add ogp

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from "./components/ui/provider"
 import App from './App.jsx'
 import './index.css'
+import Seo from "@/components/common/Seo";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
