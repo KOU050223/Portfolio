1bf3d8ae24bb7b44243ba7fe4fc38ccd233d3861
KOU050223 <kouhei20050223@icloud.com>
2025-07-26 22:41:22 +0900

    feat: 画像設定を追加し、セキュリティポリシーを更新する

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,22 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/profile_images/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'skillicons.dev',
+        port: '',
+        pathname: '/icons/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/vi/**',
+      },
     ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
